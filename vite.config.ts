0a1f4615ecf883a4b7378828f71680615d2577df
sutandi azhari <sutandi.azhari@ai.astra.co.id>
2025-10-07 18:31:40 +0700

    build: output to docs for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/github-repos-explorer/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
